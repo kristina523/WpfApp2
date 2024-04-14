85524b1577874e173a2463305e3f0ee734b48082
Артём <@DESKTOP-1MI1908>
2024-04-14 21:00:11 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/Buyers.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace ABBA
+{
+
+    public partial class Buyers : Page
+    {
+        private WILDBEREntities context = new WILDBEREntities();
+        public Buyers()
+        {
+            InitializeComponent();
+            BuyersDateGrid.ItemsSource =context.Buyers.ToList();
+        }
+    }
+}
